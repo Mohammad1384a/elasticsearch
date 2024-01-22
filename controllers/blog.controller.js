6507522b92b06f72fd732f2b2945914f1e45a266
mohammad <abedinimcf@gmail.com>
2024-01-22 13:39:49 -0600

    get all and remove blog added

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -18,6 +18,43 @@ async function createBlog(req, res, next) {
   }
 }
 
+async function getAllBlogs(req, res, next) {
+  try {
+    const { value } = req.params;
+    const blogs = await elasticClient.search({
+      index: "blog",
+      q: value,
+    });
+    //search by id
+    // const blogId = await elasticClient.get({
+    //   index: "blog",
+    //   id: value,
+    // });
+    return res.json(blogs.hits.hits);
+  } catch (error) {
+    return next(createError.InternalServerError(error));
+  }
+}
+
+async function removeBlogById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const deletionResult = await elasticClient.deleteByQuery({
+      index: "blog",
+      query: {
+        match: {
+          _id: id,
+        },
+      },
+    });
+    return res.json(deletionResult);
+  } catch (error) {
+    return next(createError.InternalServerError(error));
+  }
+}
+
 module.exports = {
   createBlog,
+  getAllBlogs,
+  removeBlogById,
 };
--- router/blog.router.js
+++ router/blog.router.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
 const router = Router();
-const { createBlog } = require("../controllers/blog.controller");
+const {
+  createBlog,
+  getAllBlogs,
+  removeBlogById,
+} = require("../controllers/blog.controller");
 
 router.post("/create", createBlog);
+router.get("/all/:value?", getAllBlogs);
+router.delete("/remove/:id", removeBlogById);
 
 module.exports = {
   blogRouter: router,
